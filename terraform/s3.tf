82efcd47203bcd3c13fdbcef8f7bf546d292a309
Gustavo Nucci <gustavov.nucci@gmail.com>
2025-08-14 21:38:01 -0300

    CHORE: S3 updated

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,11 +1,3 @@
-resource "aws_s3_bucket" "bronze-bucket" {
-  bucket = "nuccig-data-analysis-bronze"
-}
-
-resource "aws_s3_bucket" "silver-bucket" {
-  bucket = "nuccig-data-analysis-silver"
-}
-
-resource "aws_s3_bucket" "gold-bucket" {
-  bucket = "nuccig-data-analysis-gold"
+resource "aws_s3_bucket" "ecommerce" {
+  bucket = "nuccig-data-analysis-ecommerce"
 }
