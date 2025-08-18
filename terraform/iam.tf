47368650395a3f07f1a186fbd5d973e0cadd5e13
Gustavo Nucci <gustavov.nucci@gmail.com>
2025-08-17 22:54:36 -0300

    FEAT: Created Data Crawlers

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -45,8 +45,84 @@ resource "aws_iam_role_policy" "airflow_ec2_s3_policy" {
   })
 }
 
-# IAM Instance Profile
+resource "aws_iam_role_policy" "airflow_ec2_glue_policy" {
+  name = "${var.project_name}-airflow-ec2-glue-policy"
+  role = aws_iam_role.airflow_ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:StartCrawler",
+          "glue:StopCrawler",
+          "glue:GetCrawler",
+          "glue:GetCrawlerMetrics",
+          "glue:BatchGetCrawlers",
+          "glue:ListCrawlers",
+          "glue:GetDatabase",
+          "glue:GetDatabases",
+          "glue:GetTable",
+          "glue:GetTables",
+          "glue:GetPartitions"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_instance_profile" "airflow_ec2_profile" {
   name = "${var.project_name}-airflow-ec2-profile"
   role = aws_iam_role.airflow_ec2_role.name
+}
+
+resource "aws_iam_role" "glue_crawler_role" {
+  name = "${var.project_name}-glue-crawler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-glue-crawler-role"
+  }
+}
+
+resource "aws_iam_role_policy" "glue_crawler_s3_policy" {
+  name = "${var.project_name}-glue-crawler-s3-policy"
+  role = aws_iam_role.glue_crawler_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.ecommerce.arn,
+          "${aws_s3_bucket.ecommerce.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_crawler_service_role" {
+  role       = aws_iam_role.glue_crawler_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
