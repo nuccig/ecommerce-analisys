fde4f05ba9697443b581d3a96b29bd48bf89ba2f
Gustavo Nucci <gustavov.nucci@gmail.com>
2025-09-07 12:41:00 -0300

    FEAT: Added gold glue script to s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,3 +14,16 @@ resource "aws_s3_object" "bronze_to_silver_script" {
     Layer       = "silver"
   }
 }
+
+resource "aws_s3_object" "silver_to_gold_script" {
+  bucket       = aws_s3_bucket.ecommerce.bucket
+  key          = "glue-scripts/silver_to_gold.py"
+  content      = file("${path.module}/scripts/silver_to_gold.py")
+  content_type = "text/plain"
+
+  tags = {
+    Name        = "silver_to_gold-script"
+    Environment = "production"
+    Layer       = "gold"
+  }
+}
